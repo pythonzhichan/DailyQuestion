f3eb58e2371ceb3b864fe41fecee71d99befa915
Lira <424361609@qq.com>
2017-11-07 09:31:40 +0800

    Update suixinlu.py

--- question_4/suixinlu.py
+++ question_4/suixinlu.py
@@ -19,3 +19,18 @@ __mtime__ = '2017/11/7'
                   ┃┫┫  ┃┫┫
                   ┗┻┛  ┗┻┛
 """
+def fib(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def each_item(items):
+    for item in range(1, items + 1):
+        print('%d,' % fib(item), end='')
+        print('......')
+        
+print(each_item(10))
