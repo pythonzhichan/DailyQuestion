ce2000066a0e7aa2021c87f642f2f9f0746d0f4d
hgleagle <hgleagle@gmail.com>
2017-11-12 20:53:04 +0800

    add matrix solution

--- question_4/heguilong.py
+++ question_4/heguilong.py
@@ -8,16 +8,18 @@ Description:
     斐波那契数列由0和1开始，之后的斐波那契系数就是由之前的两数相加而得出，例如
 斐波那契数列的前10个数是 0, 1, 1, 2, 3, 5, 8, 13, 21, 34。
 """
-import sys
 import logging
+import sys
+
+import numpy as np
 
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s \
                     - %(message)s')
 
 
-# solution 1
-class Fibonaci():
+# solution 1, O(1.618^n)
+class Fibonacci():
     def __init__(self):
         """TODO: Docstring for __init__.
 
@@ -36,7 +38,8 @@ class Fibonaci():
 
         """
         # logging.debug("number: {%d}" % number)
-        if number <= 1:
+        assert number >= 0
+        if number in (0, 1):
             result = number
         else:
             result = self.calculate(number - 1) + self.calculate(number - 2)
@@ -48,13 +51,18 @@ class Fibonaci():
         print(self.fib_dict.values())
 
 
-# solution 2
-def fib(n):
+# solution 2, O(n)
+def fib_loop(n):
     a, b = 0, 1
     while n > 0:
         yield b
         a, b = b, a + b
         n -= 1
+
+
+# solution 3, O(logn)
+def fib_matr(n):
+    return (np.matrix([1, 1], [1, 0]) ** (n - 1) * np.matrix([1], [0]))[0, 0]
 
 
 if __name__ == "__main__":
@@ -67,11 +75,16 @@ if __name__ == "__main__":
 
     # solution 1
     print("Solution 1:")
-    fib_obj = Fibonaci()
+    fib_obj = Fibonacci()
     fib_obj.calculate(number)
     fib_obj.show_fib_values()
 
     # solution 2
     print("Solution 2:")
-    for i in fib(number):
-        print(i)
+    for i in fib_loop(number):
+        print(i, end=" ")
+
+    # solution 3
+    print("Solution 3:")
+    for n in range(number):
+        print(fib_matr(n), end=" ")
