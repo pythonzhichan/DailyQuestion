8b5f53bef72984170a4b574a9e2fc30bd73db498
heshuaidi <heshuaidi123@gmail.com>
2017-12-04 23:47:51 +0800

    add some comments

--- shuaidi/question01.py
+++ shuaidi/question01.py
@@ -8,13 +8,15 @@ def print_index_value_pair_for_list(in_list):
         print("()")
         return None
 
-    list_for_print = []
+    list_for_print = [] # prepare elements to print
     for i in range(0, length-1):
         list_for_print.append(f"({i}, {in_list[i]}), ")
 
+    # handle the last element
     list_for_print.append(f"({length-1}, {in_list[length-1]})")
 
     print("".join(list_for_print))
 
+# invoke to test print_index_value_pair_for_list()
 in_param = ["a", "b", "c"]
 print_index_value_pair_for_list(in_param)
